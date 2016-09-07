7cb4329550fcc9bcfad7b40aa52e77478879e014
Dylan Chew <dylanchew@203-213.priv19.nus.edu.sg>
2016-09-07 09:29:02 +0800

    add Printable Interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface to get a printable string of contact details
+ *
+ */
+public interface Printable {
+	
+	public String getPrintableString();
+
+}
